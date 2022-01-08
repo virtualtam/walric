707347a8510d1841aacdb5964415dc5eb839fe8c
VirtualTam <virtualtam@flibidi.net>
2022-01-08 14:22:20 +0100

    config: remove client ID and secret (read-only)
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- config.go
+++ config.go
@@ -16,14 +16,12 @@ func (c *Config) DatabasePath() string {
 }
 
 type redditInfo struct {
-	UserAgent    string `toml:"user_agent"`
-	ClientID     string `toml:"client_id"`
-	ClientSecret string `toml:"client_secret"`
+	UserAgent string `toml:"user_agent"`
 }
 
 type redwallInfo struct {
 	DataDir         string   `toml:"data_dir"`
-	SubmissionLimit string   `toml:"submission_limit"`
+	SubmissionLimit int      `toml:"submission_limit"`
 	TimeFilter      string   `toml:"time_filter"`
 	Subreddits      []string `toml:"subreddits"`
 }
