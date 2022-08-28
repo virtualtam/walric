a1857bf29228bd2435c2387f4b8b8bde20745feb
VirtualTam <virtualtam@flibidi.net>
2022-08-28 23:34:16 +0200

    gather: split logic
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- gather/gatherer.go
+++ gather/gatherer.go
@@ -86,6 +86,104 @@ func (g *Gatherer) filterPosts(posts []*reddit.Post) ([]*reddit.Post, error) {
 	return imagePosts, nil
 }
 
+func (g *Gatherer) gatherImageSubmission(dbSubreddit *subreddit.Subreddit, subredditName string, subredditDir string, post *reddit.Post) error {
+	postImage, err := newPostImage(subredditDir, post)
+	if err != nil {
+		log.Error().Err(err).Msgf("%s: failed to fetch image metadata from URL: %s", subredditName, post.URL)
+		return err
+	}
+
+	if err := postImage.Download(); err != nil {
+		log.Error().Err(err).Msgf("%s: failed to download image from URL: %s", subredditName, post.URL)
+		return err
+	}
+
+	err = postImage.UpdateResolution()
+	if errors.Is(err, image.ErrFormat) {
+		log.Warn().Msgf("%s: unknown or unsupported image file format: %s", subredditName, postImage.filePath)
+
+		if err := os.Remove(postImage.filePath); err != nil {
+			log.Error().Err(err).Msgf("image: failed to remove file: %s", postImage.filePath)
+			return err
+		}
+
+		log.Warn().Msgf("%s: file removed: %s", subredditName, postImage.filePath)
+
+		return nil
+	} else if err != nil {
+		log.Error().Err(err).Msgf("%s: failed to get image resolution: %s", subredditName, postImage.filePath)
+		return err
+	}
+
+	imageURL, err := url.Parse(post.URL)
+	if err != nil {
+		log.Error().Err(err).Msgf("%s: failed to parse image URL: %s", subredditName, imageURL)
+		return err
+	}
+
+	dbSubmission := &submission.Submission{
+		Subreddit:     dbSubreddit,
+		SubredditID:   dbSubreddit.ID,
+		Author:        post.Author,
+		Permalink:     post.Permalink,
+		PostID:        post.ID,
+		PostedAt:      post.Created.UTC(),
+		Score:         post.Score,
+		Title:         post.Title,
+		ImageDomain:   imageURL.Host,
+		ImageURL:      post.URL,
+		ImageNSFW:     post.NSFW,
+		ImageFilename: postImage.filePath,
+		ImageHeightPx: postImage.HeightPx,
+		ImageWidthPx:  postImage.WidthPx,
+	}
+
+	if err := g.submissionService.Create(dbSubmission); err != nil {
+		log.Error().Err(err).Msgf("%s: failed to create submission entry: %s - %s", subredditName, post.ID, post.Title)
+		return err
+	}
+
+	log.Info().Msgf("%s: submission saved to database: %s - %s", subredditName, post.ID, post.Title)
+	return nil
+}
+
+func (g *Gatherer) gatherImageSubmissions(ctx context.Context, subredditName string, posts []*reddit.Post) error {
+	subredditDir := filepath.Join(g.dataDir, subredditName)
+	if err := os.MkdirAll(subredditDir, os.ModePerm); err != nil {
+		log.Error().Err(err).Msgf("failed to create directory: %s", subredditDir)
+		return err
+	}
+
+	dbSubreddit, err := g.subredditService.ByName(subredditName)
+
+	if errors.Is(err, subreddit.ErrNotFound) {
+		log.Info().Msgf("%s: save subreddit information to database", subredditName)
+
+		dbSubreddit = &subreddit.Subreddit{Name: subredditName}
+		if err = g.subredditService.Create(dbSubreddit); err != nil {
+			log.Error().Err(err).Msgf("%s: failed to create database entry", subredditName)
+			return err
+		}
+
+		dbSubreddit, err = g.subredditService.ByName(subredditName)
+		if err != nil {
+			log.Error().Err(err).Msg("database: failed to retrieve subreddit")
+			return err
+		}
+	} else if err != nil {
+		log.Error().Err(err).Msg("database: failed to query subreddit information")
+		return err
+	}
+
+	for _, post := range posts {
+		if err := g.gatherImageSubmission(dbSubreddit, subredditName, subredditDir, post); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (g *Gatherer) GatherTopImageSubmissions(ctx context.Context, subredditNames []string, listPostOptions *reddit.ListPostOptions) error {
 	for _, subredditName := range subredditNames {
 		topPosts, _, err := g.client.Subreddit.TopPosts(
@@ -129,92 +227,8 @@ func (g *Gatherer) GatherTopImageSubmissions(ctx context.Context, subredditNames
 			listPostOptions.Time,
 		)
 
-		subredditDir := filepath.Join(g.dataDir, subredditName)
-		if err := os.MkdirAll(subredditDir, os.ModePerm); err != nil {
-			log.Error().Err(err).Msgf("failed to create directory: %s", subredditDir)
+		if err := g.gatherImageSubmissions(ctx, subredditName, posts); err != nil {
 			return err
-		}
-
-		dbSubreddit, err := g.subredditService.ByName(subredditName)
-
-		if errors.Is(err, subreddit.ErrNotFound) {
-			log.Info().Msgf("%s: save subreddit information to database", subredditName)
-
-			dbSubreddit = &subreddit.Subreddit{Name: subredditName}
-			if err = g.subredditService.Create(dbSubreddit); err != nil {
-				log.Error().Err(err).Msgf("%s: failed to create database entry", subredditName)
-				return err
-			}
-
-			dbSubreddit, err = g.subredditService.ByName(subredditName)
-			if err != nil {
-				log.Error().Err(err).Msg("database: failed to retrieve subreddit")
-				return err
-			}
-		} else if err != nil {
-			log.Error().Err(err).Msg("database: failed to query subreddit information")
-			return err
-		}
-
-		for _, post := range posts {
-			postImage, err := newPostImage(subredditDir, post)
-			if err != nil {
-				log.Error().Err(err).Msgf("%s: failed to fetch image metadata from URL: %s", subredditName, post.URL)
-				return err
-			}
-
-			if err := postImage.Download(); err != nil {
-				log.Error().Err(err).Msgf("%s: failed to download image from URL: %s", subredditName, post.URL)
-				return err
-			}
-
-			err = postImage.UpdateResolution()
-			if errors.Is(err, image.ErrFormat) {
-				log.Warn().Msgf("%s: unknown or unsupported image file format: %s", subredditName, postImage.filePath)
-
-				if err := os.Remove(postImage.filePath); err != nil {
-					log.Error().Err(err).Msgf("image: failed to remove file: %s", postImage.filePath)
-					return err
-				}
-
-				log.Warn().Msgf("%s: file removed: %s", subredditName, postImage.filePath)
-
-				continue
-			}
-			if err != nil {
-				log.Error().Err(err).Msgf("%s: failed to get image resolution: %s", subredditName, postImage.filePath)
-				return err
-			}
-
-			imageURL, err := url.Parse(post.URL)
-			if err != nil {
-				log.Error().Err(err).Msgf("%s: failed to parse image URL: %s", subredditName, imageURL)
-				return err
-			}
-
-			dbSubmission := &submission.Submission{
-				Subreddit:     dbSubreddit,
-				SubredditID:   dbSubreddit.ID,
-				Author:        post.Author,
-				Permalink:     post.Permalink,
-				PostID:        post.ID,
-				PostedAt:      post.Created.UTC(),
-				Score:         post.Score,
-				Title:         post.Title,
-				ImageDomain:   imageURL.Host,
-				ImageURL:      post.URL,
-				ImageNSFW:     post.NSFW,
-				ImageFilename: postImage.filePath,
-				ImageHeightPx: postImage.HeightPx,
-				ImageWidthPx:  postImage.WidthPx,
-			}
-
-			if err := g.submissionService.Create(dbSubmission); err != nil {
-				log.Error().Err(err).Msgf("%s / %s: failed to create submission entry", subredditName, post.ID)
-				return err
-			}
-
-			log.Info().Msgf("%s: submission saved to database: %s - %s", subredditName, post.ID, post.Title)
 		}
 	}
 
